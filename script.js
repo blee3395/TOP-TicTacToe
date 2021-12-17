ced852ee16fd95066cb210836bfb04249242286b
Brennan <blee3395@gmail.com>
2021-12-16 22:35:37 -0500

    Working version with alternating player turns, Alert for winner display, Reset button for clearing board, Needs work on Player vs Computer

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const contents = document.querySelector('.contents');
 
+let turn = true;
+let count = 0;
+
 const gameBoard = (() => {
-    
+    // const human = document.querySelector('.human');
+    // const computer = document.querySelector('.computer');
+
+    // human.addEventListener('click', () => {
+        
+    // })
+
+    const resetBtn = document.querySelector('.resetBtn');
+    resetBtn.addEventListener('click', () => {
+        for (let i = 0; i < 9; i++)
+        {
+            let sq = document.querySelector('.sq'+i);
+            if (sq.hasChildNodes()) {
+                let img = document.querySelector('.img');
+                sq.removeChild(img);  
+            }
+        }
+        turn = true;
+        count = 0;
+    });
+
     const initBoard = () => {
         const gameBoard = document.createElement('div');
         gameBoard.className = 'gameBoard';
@@ -65,21 +88,34 @@ const square = (num) => {
     return sq;
 };
 
+const nextTurn = () => {
+    count++;
+    return turn = ((turn) ? false: true);
+}
+
 const move = (e) => {
     let sq = document.querySelector('.'+ e.target.className);
     
     //Check move if possible
     if (sq.hasChildNodes()) {
-        alert('There is already shit there you dumbass.')
+        alert('There is already shit there you dumbass.');
     }
     else {
         //Make move
         sq.appendChild(setValue());
 
         //Check if winner
+        if (check() != null) {
+            console.log(check());
+            endGame();
+        }
+
+        if (count == 8) {
+            endGame();
+        }
 
         //Next player turn
-
+        nextTurn();
     }
    
 }
@@ -92,16 +128,97 @@ const setValue = (player) => {
         height: 90px;
     `;
 
-    //TODO
-    //Check player turn
-
-    //Set X
-    img.src = './src/x.png';
-
-    //Set O
-    // img.src = './src/o.png';
+    //Check player turn, Assign X or O
+    if (turn) {
+        img.src = './src/x.png';
+        img.className = 'img X'
+    } 
+    else {
+        img.src = './src/o.png';
+        img.className = 'img O';
+    };
 
     return img;
+}
+
+const check = () => {
+    let arr = [];
+    for (let i = 0; i < 9; i++) {
+        let sq = document.querySelector('.sq'+i);
+        if (sq.hasChildNodes()) {
+            let value = sq.querySelector('.img').className;
+            if (/X/.test(value)) {
+                arr.push(true);
+            }
+            else if (/O/.test(value)) {
+                arr.push(false);
+            }
+        }
+        else {
+            arr.push(null);
+        }
+    }
+
+    if (arr[0] == arr[3] && arr[0] == arr[6]) 
+    {
+        if (arr[0] == null) {
+            return
+        }
+        return (arr[0]) ? 'X' : 'O';
+    }
+    if (arr[1] == arr[4] && arr[1] == arr[7]) 
+    {
+        if (arr[1] == null) {
+            return
+        }
+        return (arr[1]) ? 'X' : 'O';
+    }
+    if (arr[2] == arr[5] && arr[2] == arr[8]) 
+    {
+        if (arr[2] == null) {
+            return
+        }
+        return (arr[2]) ? 'X' : 'O';
+    }
+    if (arr[0] == arr[1] && arr[0] == arr[2]) 
+    {
+        if (arr[0] == null) {
+            return
+        }
+        return (arr[0]) ? 'X' : 'O';
+    }
+    if (arr[3] == arr[4] && arr[3] == arr[5]) 
+    {
+        if (arr[3] == null) {
+            return
+        }
+        return (arr[3]) ? 'X' : 'O';
+    }
+    if (arr[6] == arr[7] && arr[6] == arr[8]) 
+    {
+        if (arr[6] == null) {
+            return
+        }
+        return (arr[6]) ? 'X' : 'O';
+    }
+    if (arr[0] == arr[4] && arr[0] == arr[8]) 
+    {
+        if (arr[0] == null) {
+            return
+        }
+        return (arr[0]) ? 'X' : 'O';
+    }
+    if (arr[2] == arr[4] && arr[2] == arr[6]) 
+    {
+        if (arr[2] == null) {
+            return
+        }
+        return (arr[2]) ? 'X' : 'O';
+    }
+}
+
+const endGame = () => {
+    alert(`Game over bro... ${check()} has won`)
 }
 
 contents.appendChild(gameBoard.initBoard());
